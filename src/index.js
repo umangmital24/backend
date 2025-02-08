cd662d7d995a21d9fd07a41541ef8ec854e81fe0
umangmittal24 <umangmittal24072003@gmail.com>
2025-02-09 03:59:02 +0530

    utils

--- src/index.js
+++ src/index.js
@@ -9,7 +9,16 @@ dotenv.config({
   path: "./env"
 });
 
-connectDB();
+connectDB()
+.then(() => {
+  app.listen(process.env.PORT|| 8000, () => {
+    console.log(`Server is running on port ${process.env.PORT}`);
+  })
+})
+.catch((error) => {
+  console.log("Mongo DB connection failed", error);
+ 
+});
 
 /*
 import express from "express";
