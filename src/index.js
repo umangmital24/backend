6d2df9bd0ffc817e327c59391505342e9df2a4d5
umangmittal24 <umangmittal24072003@gmail.com>
2025-02-08 23:30:56 +0530

    database connection

---
+++ src/index.js
@@ -0,0 +1,36 @@
+//require("dotenv").config({path: './env'});
+import dotenv from "dotenv";
+
+//import mongoose from "mongoose";
+//import { DB_NAME } from "./constants";
+import connectDB from "./db/index.js";
+
+dotenv.config({ 
+  path: "./env"
+});
+
+connectDB();
+
+/*
+import express from "express";
+cont app = express();
+
+(()=>{
+    try{
+       await mongoose.connect(`${process.env.MONGO_URI}/${DB_NAME}`)
+       app.on("error",(error)=>{
+              console.log("Error: ",error);
+            throw error
+       })
+         app.listen(process.env.PORT,()=>{
+              console.log(`Server is running on port ${process.env.PORT}`);
+         })
+    }
+    catch(error)
+    {
+        console.error("Error: ",error);
+        throw err
+    }
+})
+    
+*/
